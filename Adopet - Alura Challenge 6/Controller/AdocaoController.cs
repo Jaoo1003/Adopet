51bfd7703eb5c57d43370db22939eefeb0470702
Jaoo1003 <vitoor1003@gmail.com>
2023-05-30 16:13:34 -0300

    criando controller e service para adocao

---
+++ Adopet - Alura Challenge 6/Controller/AdocaoController.cs
@@ -0,0 +1,42 @@
+﻿using Adopet___Alura_Challenge_6.Data.Dtos.Adocoes;
+using Adopet___Alura_Challenge_6.Services;
+using FluentResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Adopet___Alura_Challenge_6.Controller {
+    [ApiController]
+    [Route("[controller]")]
+    public class AdocaoController : ControllerBase{
+        private AdocaoService _service;
+        public AdocaoController(AdocaoService service) {
+            _service = service;
+        }
+
+        [HttpPost]
+        public IActionResult CadastraAdocao([FromBody] CreateAdocaoDto dto) {
+            var result = _service.CadastraAdocao(dto);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public IActionResult BuscaAdocoes() {
+            var adocoes = _service.BuscaAdocoes();
+            if (adocoes != null) return Ok(adocoes);
+            return NotFound();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscaAdocoesPorId(int id) {
+            var adocoes = _service.BuscaAdocoesPorId(id);
+            if (adocoes != null) return Ok(adocoes);
+            return NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletaAdocao(int id) {
+            Result result = _service.DeletaAdocao(id);
+            if (result.IsSuccess) return StatusCode(204, "Sucesso ao deletar adoção");
+            return NotFound("Não foi possivel deletar esta adoção");
+        }
+    }
+}
---
+++ Adopet - Alura Challenge 6/Services/AdocaoService.cs
@@ -0,0 +1,57 @@
+﻿using Adopet___Alura_Challenge_6.Data.Dtos.Adocoes;
+using Adopet___Alura_Challenge_6.Data.Ef_Core;
+using Adopet___Alura_Challenge_6.Models;
+using AutoMapper;
+using FluentResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace Adopet___Alura_Challenge_6.Services {
+    public class AdocaoService {
+        private AppDbContext _context;
+        private IMapper _mapper;
+
+        public AdocaoService(AppDbContext context, IMapper mapper) {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public ReadAdocaoDto CadastraAdocao(CreateAdocaoDto dto) {
+            var adocao = _mapper.Map<Adocao>(dto);
+            _context.Add(adocao);
+
+            var pet = _context.Pets.Where(pet => pet.Id == dto.PetId).First();
+            pet.Adotado = true;
+            _context.SaveChanges();
+
+            return _mapper.Map<ReadAdocaoDto>(adocao);
+        }
+
+        public Result DeletaAdocao(int id) {
+            var adocao = _context.Adocoes.FirstOrDefault(adocao => adocao.Id == id);
+            if (adocao == null) return Result.Fail("Não encontrado");
+            _context.Remove(adocao);
+
+            Pet pet = _context.Pets.Where(pet => pet.Id == adocao.PetId).First();
+            pet.Adotado = false;
+
+            _context.SaveChanges();
+            return Result.Ok();
+        }
+
+        public IEnumerable<ReadAdocaoDto> BuscaAdocoes() {
+            var adocoes = _context.Adocoes;
+            if (adocoes != null) {
+                return _mapper.Map<List<ReadAdocaoDto>>(adocoes);
+            }
+            return null;
+        }
+
+        public ReadAdocaoDto BuscaAdocoesPorId(int id) {
+            var adocao = _context.Adocoes.FirstOrDefault(a => a.Id == id);
+            if (adocao != null) {
+                return _mapper.Map<ReadAdocaoDto>(adocao);
+            }
+            return null
+        }
+    }
+}
