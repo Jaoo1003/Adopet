6a52a33bb34135380310b6b99e2c505839fd24b3
Jaoo1003 <vitoor1003@gmail.com>
2023-05-31 16:51:16 -0300

    Adicionando login e token

--- AdopetUsuario/Services/LoginService.cs
+++ AdopetUsuario/Services/LoginService.cs
@@ -5,9 +5,11 @@ using Microsoft.AspNetCore.Identity;
 namespace AdopetUsuario.Services {
     public class LoginService {
         private SignInManager<Usuario> _signInManager;
+        private TokenService _tokenService;
 
-        public LoginService(SignInManager<Usuario> signInManager) {
+        public LoginService(SignInManager<Usuario> signInManager, TokenService tokenService) {
             _signInManager = signInManager;
+            _tokenService = tokenService;
         }
         public async Task Login(LoginUsuarioDto dto) {
             var resultado = await _signInManager.PasswordSignInAsync(dto.Username, dto.Password, false, false);
@@ -15,6 +17,8 @@ namespace AdopetUsuario.Services {
             if (!resultado.Succeeded) {
                 throw new ApplicationException("Usuário não autenticado!");
             }
+
+            _tokenService.GenerateToken(Usuario);
         }
     }
 }
---
+++ AdopetUsuario/Services/TokenService.cs
@@ -0,0 +1,26 @@
+﻿using AdopetUsuario.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AdopetUsuario.Services {
+    public class TokenService {
+        public void GenerateToken(Usuario usuario) {
+            Claim[] claims = new Claim[] {
+                new Claim("username", usuario.UserName),
+                new Claim("id", usuario.Id)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("gShqSHJMK4hyt$98¨#&*013h5g0´WR3hH9u-9h1u43$590hj"));
+
+            var signinCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                expires: DateTime.Now.AddHours(8),
+                claims: claims,
+                signingCredentials: signinCredentials
+                );
+        }
+    }
+}
